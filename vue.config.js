b781f7dac6c1bd3d217f8c6e8287d3814530ca95
kefir <mailkoter1@gmail.com>
2024-06-19 15:41:01 +0500

    sdfsdf

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
-module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/vuefood/'
-        : '/'
-}
+const {defineConfig} = require("vite");
+module.exports = defineConfig({
+    publicPath: '/vuefood/'
+})
